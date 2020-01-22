c56cb855bc149cce8b2813f7f291c9744686cb29
Liudmyla Liashuk <lyudmyla.horbyk@gmail.com>
2020-01-22 22:20:43 +0200

    split Main.vue component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,47 @@
 <template>
-  <Main />
+  <div>
+    <Header />
+    <Main />
+    <Footer />
+  </div>
 </template>
 
 <script>
 import Main from "./components/Main.vue";
-
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 export default {
   components: {
-    Main
+    Main,
+    Header,
+    Footer
   }
 };
 </script>
+<style lang="css">
+/* general */
+
+header,
+main,
+footer,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p {
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  max-width: 1200px;
+  margin: auto;
+  font-family: "Exo 2", sans-serif;
+  padding: 7px;
+}
+* {
+  box-sizing: border-box;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <header class="header">
+    <h1 class="header__title">Pokedex</h1>
+  </header>
+</template>
+<style scoped lang="css">
+/* header */
+
+.header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.header__title {
+  border: 1px solid black;
+  font-size: 50px;
+  margin-bottom: 15px;
+  padding: 0 5px 0 5px;
+}
+@media (min-width: 540px) {
+  .header__title {
+    padding: 8px 80px 8px 80px;
+  }
+}
+</style>
